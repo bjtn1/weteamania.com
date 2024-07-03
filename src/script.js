22f93d2d59acbc7d5e9853e0fa132453c54f4a82
Brandon J. T. Noguera <58859142+bjtn1@users.noreply.github.com>
2024-07-03 16:37:05 -0400

    Refactor structure. Add teamania logos

---
+++ src/script.js
@@ -0,0 +1,11 @@
+
+// hamburger menu thingie toggle
+// document.addEventListener("DOMContentLoaded", function () {
+//   const menuToggle = document.getElementById("menu-toggle");
+//   const menu = document.getElementById("ham-menu");
+//
+//   menuToggle.addEventListener("click", function () {
+//     menu.classList.toggle("hidden");
+//   });
+// });
+
