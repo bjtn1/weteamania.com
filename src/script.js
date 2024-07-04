dbffa7a0bafa6c1fd4e9959d576906564615986b
Brandon J. T. Noguera <58859142+bjtn1@users.noreply.github.com>
2024-07-04 02:28:35 -0400

    Clicking hamburger button displays the navbar items

--- src/script.js
+++ src/script.js
@@ -2,6 +2,15 @@ function animate_ham_menu_icon(x) {
   x.classList.toggle("change");
 }
 
+function show_nav_items() {
+  let x = document.getElementById("nav-items");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
 // hamburger menu thingie toggle
 // document.addEventListener("DOMContentLoaded", function () {
 //   const menuToggle = document.getElementById("menu-toggle");
